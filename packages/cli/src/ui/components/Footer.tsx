19d552e836a0df5e44f20fe5e2e1b13052569022
jlove29 <juliette29love@gmail.com>
2025-04-20 20:16:19 +0100

    Run format

--- packages/cli/src/ui/components/Footer.tsx
+++ packages/cli/src/ui/components/Footer.tsx
@@ -14,12 +14,18 @@ interface FooterProps {
   debugMessage: string;
 }
 
-export const Footer: React.FC<FooterProps> = ({ queryLength, debugMode, debugMessage }) => (
+export const Footer: React.FC<FooterProps> = ({
+  queryLength,
+  debugMode,
+  debugMessage,
+}) => (
   <Box marginTop={1} justifyContent="space-between">
     <Box minWidth={15}>
       <Text color={Colors.SubtleComment}>
         {queryLength === 0 ? '? for shortcuts' : ''}
-        {debugMode && <Text color="red"> {debugMessage || "Running in debug mode."}</Text>}
+        {debugMode && (
+          <Text color="red"> {debugMessage || 'Running in debug mode.'}</Text>
+        )}
       </Text>
     </Box>
     <Text color={Colors.AccentBlue}>Gemini</Text>
