c76b378d0bbe68479e74f4f8d36f3aff378f196c
Allen Hutchison <adh@google.com>
2025-05-12 12:33:15 -0700

    Adding the grouding metadata for web search to the response we pass to the model.

--- packages/server/src/utils/generateContentResponseUtilities.ts
+++ packages/server/src/utils/generateContentResponseUtilities.ts
@@ -9,6 +9,14 @@ import { GenerateContentResponse } from '@google/genai';
 export function getResponseText(
   response: GenerateContentResponse,
 ): string | undefined {
+  if (response.candidates?.[0]?.groundingMetadata?.searchEntryPoint) {
+    const metadata = response.candidates[0].groundingMetadata.searchEntryPoint.renderedContent;
+    let fullresponse = response.candidates?.[0]?.content?.parts
+        ?.map((part) => part.text)
+        .join('') || undefined
+    fullresponse = fullresponse + '\n\n' + metadata;
+    return fullresponse;
+  }
   return (
     response.candidates?.[0]?.content?.parts
       ?.map((part) => part.text)
