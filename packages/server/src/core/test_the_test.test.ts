232612098c9ba7f87fbc2882d9c38248d244232f
Seth Troisi <sethtroisi@google.com>
2025-05-19 20:39:17 +0000

    test CI with new test

---
+++ packages/server/src/core/test_the_test.test.ts
@@ -0,0 +1,13 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { describe, it, expect } from 'vitest';
+
+describe('Test Test', () => {
+  it('test expect', () => {
+    expect(1 + 1).toBe(2);
+  });
+});
