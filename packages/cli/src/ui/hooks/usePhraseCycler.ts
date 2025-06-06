34e784952dae9a385922edd24b232a0178524679
Chris Perry <cperry@google.com>
2025-06-06 14:35:39 -0700

    adding very important phrases

--- packages/cli/src/ui/hooks/usePhraseCycler.ts
+++ packages/cli/src/ui/hooks/usePhraseCycler.ts
@@ -127,6 +127,9 @@ export const WITTY_LOADING_PHRASES = [
   'Enhancing... Enhancing... Still loading.',
   "It's not a bug, it's a feature... of this loading screen.",
   'Have you tried turning it off and on again? (The loading screen, not me.)',
+  "I'm Feeling Lucky",
+  "Painting the serifs back on...",
+  "Navigating the slime mold..."
 ];
 
 export const PHRASE_CHANGE_INTERVAL_MS = 15000;
