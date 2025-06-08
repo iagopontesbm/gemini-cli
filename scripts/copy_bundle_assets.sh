fab40ccb480ef75e00ce5eaa204d06aeb35fa4dd
mkorwel <matt.korwel@gmail.com>
2025-06-08 01:04:51 -0700

    fix: bundle tree-sitter native modules

--- scripts/copy_bundle_assets.sh
+++ scripts/copy_bundle_assets.sh
@@ -10,11 +10,4 @@ cp "packages/core/src/tools/shell.json" "bundle/shell.json"
 # Find and copy all .sb files from packages to the root of the bundle directory
 find packages -name '*.sb' -exec cp -f {} bundle/ \;
 
-# Find and copy all .node files from node_modules/tree-sitter-*/build/Release to the root of the bundle directory
-for file in node_modules/tree-sitter-*/build/Release/*.node; do
-  if [ -f "$file" ]; then
-    cp -f "$file" bundle/
-  fi
-done
-
 echo "Assets copied to bundle/"
