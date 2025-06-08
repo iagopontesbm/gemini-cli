34dd1dc9109b7c6ff42f3299c559f3d7b6d478de
mkorwel <matt.korwel@gmail.com>
2025-06-08 01:12:40 -0700

    fix: mark tree-sitter as external in esbuild.config.js

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,34 +1,5 @@
 import esbuild from 'esbuild';
 
-const nativeNodeModulesPlugin = {
-  name: 'native-node-modules',
-  setup(build) {
-    // If a ".node" file is imported within a module in the "file" namespace, resolve
-    // it to an absolute path and put it into the "node-file" virtual namespace.
-    build.onResolve({ filter: /\.node$/, namespace: 'file' }, args => ({
-      path: require.resolve(args.path, { paths: [args.resolveDir] }),
-      namespace: 'node-file',
-    }))
-
-    // Files in the "node-file" virtual namespace resolve to themselves.
-    build.onResolve({ filter: /.*/, namespace: 'node-file' }, args => ({
-      path: args.path,
-      namespace: 'node-file',
-    }))
-
-    // When a ".node" file is loaded, we want to actually use the file loader
-    // to copy the file to the output directory.
-    build.onLoad({ filter: /.*/, namespace: 'node-file' }, args => ({
-      contents: `
-        import path from ${JSON.stringify(args.path)}
-        try { module.exports = require(path) }
-        catch {}
-      `,
-      loader: 'js',
-    }))
-  },
-}
-
 esbuild.build({
   entryPoints: ['packages/cli/index.ts'],
   bundle: true,
@@ -38,5 +9,5 @@ esbuild.build({
   banner: {
     js: `import { createRequire } from 'module'; const require = createRequire(import.meta.url); globalThis.__filename = require('url').fileURLToPath(import.meta.url); globalThis.__dirname = require('path').dirname(globalThis.__filename);`,
   },
-  plugins: [nativeNodeModulesPlugin],
+  external: ['tree-sitter'],
 }).catch(() => process.exit(1));
