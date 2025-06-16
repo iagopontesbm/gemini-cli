bb2cf0c924bb24989ca8c0a14168775609e704b8
mkorwel <matt.korwel@gmail.com>
2025-06-15 21:07:17 -0700

    formatting

--- packages/cli/src/utils/sandbox.ts
+++ packages/cli/src/utils/sandbox.ts
@@ -505,7 +505,6 @@ export async function start_sandbox(sandbox: string) {
       args.push('--env', `no_proxy=${noProxy}`);
     }
 
-
     // if using proxy, switch to internal networking through proxy
     if (proxy) {
       execSync(
