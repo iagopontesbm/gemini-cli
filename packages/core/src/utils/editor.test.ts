260835edd78f27e58b16b9d0806565a18fd3a032
cornmander <shikhman@google.com>
2025-06-24 02:26:03 -0400

    Update editor.test.ts
    fix fix lint

--- packages/core/src/utils/editor.test.ts
+++ packages/core/src/utils/editor.test.ts
@@ -176,7 +176,7 @@ describe('editor utils', () => {
       expect(spawn).toHaveBeenCalledWith(
         'code',
         ['--wait', '--diff', 'old.txt', 'new.txt'],
-        { stdio: 'inherit', shell: true, },
+        { stdio: 'inherit', shell: true },
       );
       expect(mockSpawn.on).toHaveBeenCalledWith('close', expect.any(Function));
       expect(mockSpawn.on).toHaveBeenCalledWith('error', expect.any(Function));
